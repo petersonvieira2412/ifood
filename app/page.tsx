e4cb1dc3f50bd45be76aac54160253043aee4fa2
Peterson <petersonvieira2412@gmail.com>
2024-05-04 22:51:53 -0300

    chore: add husky and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
-    <div className="bg-red-500 text-white px-4">
+    <div className="bg-red-500 px-4 text-white">
       <h1>Hello to Ifood!</h1>
     </div>
   );
-}
+};
 
 export default Home;
